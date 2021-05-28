1faa115efd5ba5dcc205bac693cbbf3c4eb3464a
aissam3210 <aissam.benkerta.7e2@itb.cat>
2021-05-28 10:28:53 +0200

    hola

---
+++ aissam.java
@@ -0,0 +1,14 @@
+package com.java.basic;
+/**
+ *
+ * @author Aissam3210
+ */
+public class Bienvenido {
+    
+    //Inicio del método main que ejecuta la aplicación
+    public static void main(String[] arg){
+        System.out.println("¡Bienvenido a la programación en JAVA!");
+    }
+    //Fin del método main
+}
+//Fin de la clase Bienvenido
